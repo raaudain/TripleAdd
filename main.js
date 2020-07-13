29af9767a7a252a826cf9a02e86cfad13ffda550
Ramon Audain <raudain123@gmail.com>
2020-07-13 18:45:37 +0000

    solution

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-function trip
+function tripleAdd(num1){
+  return function(num2){
+    return function(num3){
+      return num1 + num2 + num3;
+    }
+  }
+}
+
+tripleAdd(10)(20)(30) // returns 60
